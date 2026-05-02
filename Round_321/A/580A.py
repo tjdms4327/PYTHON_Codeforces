deb7fc971215af592f60c294d23a9c11733cb9d5
tjdms4327 <tjdms4327@gmail.com>
2026-05-02 13:56:17 +0900

    Create 580A.py

---
+++ Round_321/A/580A.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+A = list(map(int, input().split()))
+
+dp = [1] * n
+for i in range(1, n):
+    if A[i] >= A[i-1]:
+        dp[i] = dp[i-1]+1
+        
+print(max(dp))
