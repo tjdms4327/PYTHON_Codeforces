092ddd4fd55afb8b7a9cd15f16d12ada81345f6c
tjdms4327 <tjdms4327@gmail.com>
2026-07-16 22:03:02 +0900

    Add files via upload

---
+++ Round944/Div.4/1971B.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    s = input().strip()
+    if len(set(s))==1:
+        print('NO')
+    else:
+        print('YES')
+        
+        new = sorted(s)
+        if new == s:
+            new = new[::-1]
+        print(''.join(new))
+        
